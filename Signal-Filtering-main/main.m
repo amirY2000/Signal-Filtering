50aecad86a22ccf35808988b46a4f71eb98d478c
Amir Yasaei <44714388+amirY2000@users.noreply.github.com>
2022-11-05 19:45:19 -0400

    Add files via upload

---
+++ Signal-Filtering-main/main.m
@@ -0,0 +1,18 @@
+clear;clc;
+% [yB,FsB] = processor('Birds.wav', 'Birds1.wav');
+% [yBcopy,FsBcopy] = audioread("Birds1.wav");
+% plot(yBcopy)
+
+[yD,FsD] = processor('Drum.wav', 'Drum1.wav');
+[yDcopy,FsDcopy] = audioread("Drum1.wav");
+subplot(2,1,1)
+plot(yDcopy)
+subplot(2,1,2)
+plot(meanFilter(10,yDcopy))
+sound(meanFilter(20,yDcopy), FsDcopy)
+
+
+
+
+%[y3,Fs3] = processor('Speech.wav');
+% audiowrite("Speech1.wav", y3, Fs3);
---
+++ Signal-Filtering-main/meanFilter.m
@@ -0,0 +1,6 @@
+function y = meanFilter(windowSize, inputSignal)
+    b = (1/windowSize)*ones(1,windowSize);
+    a = 1;
+    y = filter(b,a,inputSignal);
+    plot(y)
+end
