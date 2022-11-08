52704d6cb59edc994299816ceaff4d9d8378407a
concon120 <71888142+concon120@users.noreply.github.com>
2022-11-08 17:09:07 -0500

    Add files via upload

--- gaussianFilter.m
+++ gaussianFilter.m
@@ -1,1 +1,10 @@
-function y = gaussianFilter(windowSize, 
+function y = gaussianFilter(windowSize, inputSignal)
+    %gets the gaussian distribution for 'window' points
+    gaussDistr = gausswin(windowSize);
+    %normalizing the weights by dividing by the sum of the gaussian distr
+    b = gaussDistr/(sum(gaussDistr));
+    %filtering the input signal
+    y = filter(b, 1, inputSignal);
+    plot(y)
+end
+
