346eab790f02298c1950a5f303fa931be295fccc
Amir Yasaei <44714388+amriyasaei@users.noreply.github.com>
2022-11-24 23:49:54 -0500

    Update processor.m

--- processor.m
+++ processor.m
@@ -1,17 +1,24 @@
 %This funciton converts stereo signal to mono and changes the sample rate
 %then eventually writes the modified signal into a new wav file
+
 function [y, Fs] = processor(filename, newFile)
+    
     [y, Fs] = audioread(filename);
     info = audioinfo(filename);
     info.SampleRate
     
     temp = y; %saving the original signal
-     if size(y,2) == 2
-          y = sum(y(:,1:2),2); %convert stereo to mono
-     end
+    
+    if size(y,2) == 2
+         y = sum(y(:,1:2),2); %convert stereo to mono
+    end
      
      %writing audio to a new file
-     audiowrite(newFile, y, 16000); %making another file with mono and resampled signal and changed frequency
-     %convert back the original signal
+     
+     audiowrite(newFile, y, 16000); 
+     
+     %making another file with mono and resampled signal and changed
+     %frequency convert back the original signal
+     
      y = temp;
 end
