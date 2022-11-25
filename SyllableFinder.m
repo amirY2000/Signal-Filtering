2e303142a76828f76029fdfba2ef5524c1aa0406
Amir Yasaei <44714388+amriyasaei@users.noreply.github.com>
2022-11-25 00:02:33 -0500

    Update SyllableFinder.m

--- SyllableFinder.m
+++ SyllableFinder.m
@@ -1,15 +1,20 @@
 %This fucntion is used to calculate the number of syllables in the speech signal
+
 function numSyllables = SyllableFinder(inputSignal)
     signal = SilentRegion(inputSignal);
     numSyllables = 0; %number of syllables
     silent = 0; %The size of silent region
+    
     for i = 1:1:size(signal)
           if (signal(i) == 0)
               silent = silent + 1;
           end
-          if (signal(i) ~= 0 && silent < 330) %330 is the minimum range of silent that we let pass
+          
+          if (signal(i) ~= 0 && silent < 330) 
+          %330 is the minimum silent region gap between syllables
               silent = 0;
           end
+          
           if (signal(i) ~= 0 && silent >= 330)
                numSyllables = numSyllables + 1;
                silent = 0;
